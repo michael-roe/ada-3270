a3c84c4bd8a0e77d652c90158597d4d7b65a8e94
Michael Roe <michael.roe@github.com>
2025-09-17 23:17:43 +0100

    Added a test for the buffer being full

--- buffer_tests.adb
+++ buffer_tests.adb
@@ -12,14 +12,14 @@ package body Buffer_Tests is
     Assert (not Is_Full (Buff), "Buffer should not be full after Clear");
   end Test_Clear;
 
-  procedure Test_Enqueue_Dequeue(T : in out Test_Cases.Test_Case'Class) is
+  procedure Test_Enqueue_Dequeue (T : in out Test_Cases.Test_Case'Class) is
     Buff : Buffer.T;
     Sent_Byte : constant Byte := 42;
     Received_Byte : Byte := 0;
     Success : Boolean;
   begin
     Clear (Buff);
-    Assert (Is_Empty(Buff), "Buffer should be empty after Clear");
+    Assert (Is_Empty (Buff), "Buffer should be empty after Clear");
     Success := Enqueue_Tail (Buff, Sent_Byte);
     Assert (Success, "Enqueue failed when buffer had space");
     Assert (not Is_Empty (Buff), "Buffer should not be empty after Enqueue");
@@ -32,11 +32,28 @@ package body Buffer_Tests is
     Assert (Is_Empty (Buff), "Buffer should be empty after Dequeue");
   end Test_Enqueue_Dequeue;
     
+  procedure Test_Full (T : in out Test_Cases.Test_Case'Class) is
+    Buff : Buffer.T;
+    Sent_Byte : Byte := 42;
+    Success : Boolean;
+  begin
+    Clear (Buff);
+    while not Is_Full (Buff) loop
+      Success := Enqueue_Tail (Buff, Sent_Byte);
+      Assert (Success, "Enqueue should succeed if buffer is not full");
+    end loop;
+    
+    Success := Enqueue_Tail (Buff, Sent_Byte);
+    Assert (not Success, "Enqueue should fail if buffer is full");
+
+  end Test_Full;
+  
   procedure Register_Tests (T : in out Buffer_Test) is
     use AUnit.Test_Cases.Registration;
   begin
     Register_Routine (T, Test_Clear'Access, "Test_Clear");
     Register_Routine (T, Test_Enqueue_Dequeue'Access, "Test_Enqueue_Dequeue");
+    Register_Routine (T, Test_Full'Access, "Test_Full");
   end Register_Tests;
 
   function Name (T: Buffer_Test) return Test_String is
