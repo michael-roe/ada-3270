058fb439db087abe651c98b4e06fb2aa20f68b8e
Michael Roe <michael.roe@github.com>
2025-09-27 12:27:40 +0100

    buffer_tests: conform to style guide

--- buffer_tests.adb
+++ buffer_tests.adb
@@ -1,64 +1,100 @@
+with Ada.Containers; use Ada.Containers;
 with AUnit.Assertions; use AUnit.Assertions;
 with Buffer; use Buffer;
 use type Buffer.Byte;
 
 package body Buffer_Tests is
 
-  procedure Test_Clear (T : in out Test_Cases.Test_Case'Class) is
-    Buff : Buffer.T;
-  begin
-    Clear (Buff);
-    Assert (Is_Empty (Buff), "Buffer should be empty after Clear");
-    Assert (not Is_Full (Buff), "Buffer should not be full after Clear");
-  end Test_Clear;
+   procedure Test_Clear (T : in out Test_Cases.Test_Case'Class) is
+      Buff : Buffer.T;
+   begin
 
-  procedure Test_Enqueue_Dequeue (T : in out Test_Cases.Test_Case'Class) is
-    Buff : Buffer.T;
-    Sent_Byte : constant Byte := 42;
-    Received_Byte : Byte := 0;
-    Success : Boolean;
-  begin
-    Clear (Buff);
-    Assert (Is_Empty (Buff), "Buffer should be empty after Clear");
-    Success := Enqueue_Tail (Buff, Sent_Byte);
-    Assert (Success, "Enqueue failed when buffer had space");
-    Assert (not Is_Empty (Buff), "Buffer should not be empty after Enqueue");
-    Assert (not Is_Full (Buff),
-      "Buffer should not be full after single Enqueue");
-    Success := Dequeue_Head (Buff, Received_Byte);
-    Assert (Success, "Dequeue failed when buffer had data");
-    Assert (Sent_Byte = Received_Byte,
-      "Dequeued value didn't match expected value");
-    Assert (Is_Empty (Buff), "Buffer should be empty after Dequeue");
-  end Test_Enqueue_Dequeue;
-    
-  procedure Test_Full (T : in out Test_Cases.Test_Case'Class) is
-    Buff : Buffer.T;
-    Sent_Byte : Byte := 42;
-    Success : Boolean;
-  begin
-    Clear (Buff);
-    while not Is_Full (Buff) loop
+      Clear (Buff);
+
+      Assert (Is_Empty (Buff),
+          "Buffer should be empty after Clear");
+
+      Assert (not Is_Full (Buff),
+          "Buffer should not be full after Clear");
+
+      Assert (Current_Use (Buff) = 0,
+          "Current_Use should be 0 after Clear");
+
+   end Test_Clear;
+
+   procedure Test_Enqueue_Dequeue (T : in out Test_Cases.Test_Case'Class) is
+      Buff : Buffer.T;
+      Sent_Byte : constant Byte := 42;
+      Received_Byte : Byte := 0;
+      Success : Boolean;
+   begin
+
+      Clear (Buff);
+
+      Assert (Is_Empty (Buff),
+         "Buffer should be empty after Clear");
+
       Success := Enqueue_Tail (Buff, Sent_Byte);
-      Assert (Success, "Enqueue should succeed if buffer is not full");
-    end loop;
-    
-    Success := Enqueue_Tail (Buff, Sent_Byte);
-    Assert (not Success, "Enqueue should fail if buffer is full");
 
-  end Test_Full;
-  
-  procedure Register_Tests (T : in out Buffer_Test) is
-    use AUnit.Test_Cases.Registration;
-  begin
-    Register_Routine (T, Test_Clear'Access, "Test_Clear");
-    Register_Routine (T, Test_Enqueue_Dequeue'Access, "Test_Enqueue_Dequeue");
-    Register_Routine (T, Test_Full'Access, "Test_Full");
-  end Register_Tests;
+      Assert (Success,
+         "Enqueue failed when buffer had space");
 
-  function Name (T: Buffer_Test) return Test_String is
-  begin
-    return Format ("Buffer_Tests");
-  end Name;
+      Assert (not Is_Empty (Buff),
+         "Buffer should not be empty after Enqueue");
+
+      Assert (not Is_Full (Buff),
+          "Buffer should not be full after single Enqueue");
+
+      Assert (Current_Use (Buff) = 1,
+         "Current_Use should be 1 after single Enqueue");
+
+      Success := Dequeue_Head (Buff, Received_Byte);
+
+      Assert (Success,
+         "Dequeue failed when buffer had data");
+
+      Assert (Sent_Byte = Received_Byte,
+          "Dequeued value didn't match expected value");
+
+      Assert (Is_Empty (Buff),
+         "Buffer should be empty after Dequeue");
+
+   end Test_Enqueue_Dequeue;
+
+   procedure Test_Full (T : in out Test_Cases.Test_Case'Class) is
+      Buff : Buffer.T;
+      Sent_Byte : constant Byte := 42;
+      Success : Boolean;
+   begin
+      Clear (Buff);
+      while not Is_Full (Buff) loop
+         Success := Enqueue_Tail (Buff, Sent_Byte);
+         Assert (Success, "Enqueue should succeed if buffer is not full");
+      end loop;
+
+      Success := Enqueue_Tail (Buff, Sent_Byte);
+      Assert (not Success, "Enqueue should fail if buffer is full");
+
+   end Test_Full;
+
+   procedure Register_Tests (T : in out Buffer_Test) is
+      use AUnit.Test_Cases.Registration;
+   begin
+
+      Register_Routine (T, Test_Clear'Access,
+         "Test_Clear");
+
+      Register_Routine (T, Test_Enqueue_Dequeue'Access,
+         "Test_Enqueue_Dequeue");
+
+      Register_Routine (T, Test_Full'Access,
+         "Test_Full");
+
+   end Register_Tests;
+
+   function Name (T : Buffer_Test) return Test_String is
+   begin
+      return Format ("Buffer_Tests");
+   end Name;
 
 end Buffer_Tests;
--- buffer_tests.ads
+++ buffer_tests.ads
@@ -3,12 +3,16 @@ with AUnit.Test_Cases; use AUnit.Test_Cases;
 
 package Buffer_Tests is
 
-  type Buffer_Test is new Test_Cases.Test_Case with null record;
+   type Buffer_Test is new Test_Cases.Test_Case with null record;
 
-  procedure Register_Tests (T: in out Buffer_Test);
+   procedure Register_Tests (T : in out Buffer_Test);
 
-  function Name (T: Buffer_Test) return Message_String;
+   function Name (T : Buffer_Test) return Message_String;
 
-  procedure Test_Clear (T : in out Test_Cases.Test_Case'Class);
+   procedure Test_Clear (T : in out Test_Cases.Test_Case'Class);
+
+   procedure Test_Enqueue_Dequeue (T : in out Test_Cases.Test_Case'Class);
+
+   procedure Test_Full (T : in out Test_Cases.Test_Case'Class);
 
 end Buffer_Tests;
