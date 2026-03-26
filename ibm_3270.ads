b2cb23a061014561a3206cab2eb7f127cc41b682
Michael Roe <michael.roe@github.com>
2026-03-26 23:18:01 +0000

    Added ibm_3270.ads

---
+++ ibm_3270.ads
@@ -0,0 +1,21 @@
+with Buffer; use type Buffer.Byte;
+
+package IBM_3270 is
+
+   --
+   --  Bits in the Write Control Character field
+   --
+
+   WCC_MDT      : constant Buffer.Byte := 16#1#;
+   WCC_Go_Ahead : constant Buffer.Byte := 16#2#;
+   WCC_Bell     : constant Buffer.Byte := 16#4#;
+   WCC_Print    : constant Buffer.Byte := 16#8#;
+   WCC_Parity   : constant Buffer.Byte := 16#80#;
+
+   IBM_Write            : constant Buffer.Byte := 16#F1#;
+   IBM_Read_Buffer      : constant Buffer.Byte := 16#F2#;
+   IBM_Write_Structured : constant Buffer.Byte := 16#F3#;
+   IBM_Write_Erase      : constant Buffer.Byte := 16#F5#;
+   IBM_Read_Modified    : constant Buffer.Byte := 16#F6#;
+
+end IBM_3270;
