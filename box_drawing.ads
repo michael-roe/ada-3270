1ebe4c9350b360d0c54c09dc167e49ae45850c85
Michael Roe <michael.roe@github.com>
2026-03-27 17:41:51 +0000

    Add box_drawing.ads

---
+++ box_drawing.ads
@@ -0,0 +1,10 @@
+package Box_Drawing is
+
+   Horizontal : constant Wide_Character := Wide_Character'Val (16#2500#);
+   Vertical   : constant Wide_Character := Wide_Character'Val (16#2502#);
+   Down_Right : constant Wide_Character := Wide_Character'Val (16#250C#);
+   Down_Left  : constant Wide_Character := Wide_Character'Val (16#2510#);
+   Up_Right   : constant Wide_Character := Wide_Character'Val (16#2514#);
+   Up_Left    : constant Wide_Character := Wide_Character'Val (16#2518#);
+
+end Box_Drawing;
