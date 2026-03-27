ab9987f02ccfc27091bd8a207c5a020c619a1299
Michael Roe <michael.roe@github.com>
2026-03-27 17:54:25 +0000

    Add test_box.adb

---
+++ test_box.adb
@@ -0,0 +1,52 @@
+with Ada.Wide_Text_IO;
+with Box_Drawing;
+
+procedure Test_Box is
+
+   F1 : Ada.Wide_Text_IO.File_Type;
+
+begin
+
+   Ada.Wide_Text_IO.Create (F1, Ada.Wide_Text_IO.Out_File,
+      "boxes.txt", "WCEM=8");
+
+   Ada.Wide_Text_IO.Put (F1, "Up Right: ");
+   Ada.Wide_Text_IO.Put (F1, Box_Drawing.Up_Right);
+   Ada.Wide_Text_IO.New_Line (F1);
+
+   Ada.Wide_Text_IO.Put (F1, "Down Right: ");
+   Ada.Wide_Text_IO.Put (F1, Box_Drawing.Down_Right);
+   Ada.Wide_Text_IO.New_Line (F1);
+
+   Ada.Wide_Text_IO.Put (F1, "Up Left: ");
+   Ada.Wide_Text_IO.Put (F1, Box_Drawing.Up_Left);
+   Ada.Wide_Text_IO.New_Line (F1);
+
+   Ada.Wide_Text_IO.Put (F1, "Down Left: ");
+   Ada.Wide_Text_IO.Put (F1, Box_Drawing.Down_Left);
+   Ada.Wide_Text_IO.New_Line (F1);
+
+   Ada.Wide_Text_IO.New_Line (F1);
+
+   Ada.Wide_Text_IO.Put (F1, Box_Drawing.Down_Right);
+   Ada.Wide_Text_IO.Put (F1, Box_Drawing.Horizontal);
+   Ada.Wide_Text_IO.Put (F1, Box_Drawing.Horizontal);
+   Ada.Wide_Text_IO.Put (F1, Box_Drawing.Horizontal);
+   Ada.Wide_Text_IO.Put (F1, Box_Drawing.Down_Left);
+   Ada.Wide_Text_IO.New_Line (F1);
+
+   Ada.Wide_Text_IO.Put (F1, Box_Drawing.Vertical);
+   Ada.Wide_Text_IO.Put (F1, " ");
+   Ada.Wide_Text_IO.Put (F1, " ");
+   Ada.Wide_Text_IO.Put (F1, " ");
+   Ada.Wide_Text_IO.Put (F1, Box_Drawing.Vertical);
+   Ada.Wide_Text_IO.New_Line (F1);
+
+   Ada.Wide_Text_IO.Put (F1, Box_Drawing.Up_Right);
+   Ada.Wide_Text_IO.Put (F1, Box_Drawing.Horizontal);
+   Ada.Wide_Text_IO.Put (F1, Box_Drawing.Horizontal);
+   Ada.Wide_Text_IO.Put (F1, Box_Drawing.Horizontal);
+   Ada.Wide_Text_IO.Put (F1, Box_Drawing.Up_Left);
+   Ada.Wide_Text_IO.New_Line (F1);
+ 
+end Test_Box;
