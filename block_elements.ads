7a6ceb378925d5015d6bfb949fe71ad199772824
Michael Roe <michael.roe@github.com>
2026-06-21 17:59:51 +0100

    Added Block_Elements

---
+++ block_elements.ads
@@ -0,0 +1,9 @@
+package Block_Elements is
+
+   Upper_Half : constant Wide_Character := Wide_Character'Val (16#2580#);
+   Lower_Half : constant Wide_Character := Wide_Character'Val (16#2584#);
+   Full       : constant Wide_Character := Wide_Character'Val (16#2588#);
+   Left_Half  : constant Wide_Character := Wide_Character'Val (16#258C#);
+   Right_Half : constant Wide_Character := Wide_Character'Val (16#2590#);
+
+end Block_Elements;
