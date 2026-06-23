95c9c59150d0ab9ce161267386511ac465bf4438
Michael Roe <michael.roe@github.com>
2026-06-23 23:04:57 +0100

    Add constants for attention identifiers

--- ibm_3270.ads
+++ ibm_3270.ads
@@ -20,9 +20,28 @@ package IBM_3270 is
 
    IBM_Write_Erase_Alternate : constant Buffer.Byte := 16#7E#;
 
+   AID_PA1   : constant Buffer.Byte := 16#6c#;
+   AID_Clear : constant Buffer.Byte := 16#6d#;
+   AID_PA2   : constant Buffer.Byte := 16#6e#;
+   AID_PA3   : constant Buffer.Byte := 16#6b#;
+   AID_PF10  : constant Buffer.Byte := 16#7a#;
+   AID_PF11  : constant Buffer.Byte := 16#7b#;
+   AID_PF12  : constant Buffer.Byte := 16#7c#;
+   AID_Enter : constant Buffer.Byte := 16#7d#;
+   AID_PF1   : constant Buffer.Byte := 16#f1#;
+   AID_PF2   : constant Buffer.Byte := 16#f2#;
+   AID_PF3   : constant Buffer.Byte := 16#f3#;
+   AID_PF4   : constant Buffer.Byte := 16#f4#;
+   AID_PF5   : constant Buffer.Byte := 16#f5#;
+   AID_PF6   : constant Buffer.Byte := 16#f6#;
+   AID_PF7   : constant Buffer.Byte := 16#f7#;
+   AID_PF8   : constant Buffer.Byte := 16#f8#;
+   AID_PF9   : constant Buffer.Byte := 16#f9#;
+
    Graphic_Escape     : constant Buffer.Byte := 16#08#;
    Set_Buffer_Address : constant Buffer.Byte := 16#11#;
    Insert_Cursor      : constant Buffer.Byte := 16#13#;
+   Duplicate          : constant Buffer.Byte := 16#1c#;
    Start_Field        : constant Buffer.Byte := 16#1d#;
    Field_Mark         : constant Buffer.Byte := 16#1e#;
 
