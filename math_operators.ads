6e2f579f7688704f13df8c33fd00c1999a259a67
Michael Roe <michael.roe@github.com>
2026-06-29 12:25:06 +0100

    Added Math_Operators

---
+++ math_operators.ads
@@ -0,0 +1,27 @@
+package Math_Operators is
+
+   Increment : constant Wide_Character := Wide_Character'Val (16#2206#);
+
+   Nabla : constant Wide_Character := Wide_Character'Val (16#2207#);
+
+   Divides : constant Wide_Character := Wide_Character'Val (16#2223#);
+
+   Logical_And : constant Wide_Character := Wide_Character'Val (16#2227#);
+
+   Logical_Or : constant Wide_Character := Wide_Character'Val (16#2228#);
+
+   Intersection : constant Wide_Character := Wide_Character'Val (16#2229#);
+
+   Union : constant Wide_Character := Wide_Character'Val (16#222A#);
+   
+   Not_Equal : constant Wide_Character := Wide_Character'Val (16#2260#);
+
+   Identical : constant Wide_Character := Wide_Character'Val (16#2261#);
+
+   Less_Than_Or_Equal : constant Wide_Character :=
+      Wide_Character'Val (16#2264#);
+
+   Greater_Than_Or_Equal : constant Wide_Character :=
+      Wide_Character'Val (16#2265#);
+
+end Math_Operators;
