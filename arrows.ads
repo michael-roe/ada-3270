4a10aa61138be7692dcb05d6ad28af2a91b300fe
Michael Roe <michael.roe@github.com>
2026-07-16 14:05:23 +0100

    Added Arrows

---
+++ arrows.ads
@@ -0,0 +1,8 @@
+package Arrows is
+
+   Leftwards_Arrow  : constant Wide_Character := Wide_Character'Val (16#2190#);
+   Upwards_Arrow    : constant Wide_Character := Wide_Character'Val (16#2191#);
+   Rightwards_Arrow : constant Wide_Character := Wide_Character'Val (16#2192#);
+   Downwards_Arrow  : constant Wide_Character := Wide_Character'Val (16#2193#);
+
+end Arrows;
