8585af8d2a953ec82fa151c02a8638e4b7ab588a
Michael Roe <michael.roe@github.com>
2026-07-18 08:42:44 +0100

    Format according to style guide

--- code_page_310-tests.adb
+++ code_page_310-tests.adb
@@ -10,32 +10,32 @@ with Byte_Text_IO;
 package body Code_Page_310.Tests is
 
    procedure Test_Round_Trip (T : in out Test_Cases.Test_Case'Class) is
-   
+
       V : Byte_Vectors.Vector;
-   
+
    begin
-   
+
       Code_Page_310.Append (V, Box_Drawing.Down_Right);
       Code_Page_310.Append (V, Box_Drawing.Down_Left);
       Code_Page_310.Append (V, Box_Drawing.Up_Right);
       Code_Page_310.Append (V, Box_Drawing.Up_Left);
 
-      Assert (Code_Page_310.To_Wide_Character(V.Element (V.First_Index + 1))
+      Assert (Code_Page_310.To_Wide_Character (V.Element (V.First_Index + 1))
          = Box_Drawing.Down_Right,
          "Round trip");
 
-      Assert (Code_Page_310.To_Wide_Character(V.Element (V.First_Index + 3))
+      Assert (Code_Page_310.To_Wide_Character (V.Element (V.First_Index + 3))
          = Box_Drawing.Down_Left,
          "Round trip");
 
-      Assert (Code_Page_310.To_Wide_Character(V.Element (V.First_Index + 5))
+      Assert (Code_Page_310.To_Wide_Character (V.Element (V.First_Index + 5))
          = Box_Drawing.Up_Right,
          "Round trip");
 
-      Assert (Code_Page_310.To_Wide_Character(V.Element (V.First_Index + 7))
+      Assert (Code_Page_310.To_Wide_Character (V.Element (V.First_Index + 7))
          = Box_Drawing.Up_Left,
          "Round trip");
-   
+
    end Test_Round_Trip;
 
    procedure Register_Tests (T : in out Code_Page_Test) is
