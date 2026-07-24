bb9b87babbe5d4397ef23f5b451a33bd070f209a
Michael Roe <michael.roe@github.com>
2026-07-24 13:06:55 +0100

    Test_Break_Handlers: Added

---
+++ test_break_handlers.adb
@@ -0,0 +1,70 @@
+with Ada.Text_IO;
+with Buffer;
+with Code_Page_500;
+with IBM_3270;
+
+package body Test_Break_Handlers is
+
+  Screen_Message : Buffer.Byte_Array := (
+      IBM_3270.IBM_Write_Erase,
+      IBM_3270.WCC_Go_Ahead);
+
+  Screen_Message_No_GA : Buffer.Byte_Array := (
+      IBM_3270.IBM_Write_Erase,
+      0);
+
+   procedure To_Physical (
+      V         : Test_Break_Handler;
+      Bytes_Out : in out Byte_Vectors.Vector) is
+   begin
+
+      if V.Counter = 0 then
+         for J in Screen_Message'Range loop
+            Bytes_Out.Append (Screen_Message (J));
+         end loop;
+         Code_Page_500.Append (Bytes_Out, "Press the ENTER key");
+      else
+         for J in Screen_Message'Range loop
+            Bytes_Out.Append (Screen_Message_No_GA (J));
+         end loop;
+         Code_Page_500.Append (Bytes_Out, "Press the ATTN key");
+     end if;
+
+   end To_Physical;
+
+   procedure From_Physical (
+      V        : in out Test_Break_Handler;
+      Bytes_In : Byte_Vectors.Vector) is
+   begin
+
+      Ada.Text_IO.Put_Line ("From_Physical called");
+      V.Counter := V.Counter + 1;
+
+   end From_Physical;
+
+   procedure Break (V : in out Test_Break_Handler) is
+   begin
+
+      Ada.Text_IO.Put_Line ("Break called");
+      V.Break_Received := True;
+
+   end Break;
+
+   procedure Initialize (V : in out Test_Break_Handler) is
+   begin
+
+      null;
+
+   end;
+
+   procedure Set_RX_TX (
+      V  : in out Test_Break_Handler;
+      RX : access Buffer_Queues.Queue;
+      TX : access Buffer_Queues.Queue) is
+   begin
+
+      null;
+
+   end Set_RX_TX;
+
+end Test_Break_Handlers;
---
+++ test_break_handlers.ads
@@ -0,0 +1,39 @@
+with Byte_Vectors;
+with Buffer_Queues;
+with Telnet.Event_Handlers;
+
+package Test_Break_Handlers is
+
+   type Test_Break_Handler is new Telnet.Event_Handlers.Handler with record
+      Counter : Natural := 0;
+      Break_Received : Boolean := False;
+   end record;
+
+   --
+   --  Test_Break_Handler is a test of Telnet BREAK.
+   --
+   --  The first panel it sends has Go Ahead set and
+   --  asks the user to press ENTER. The second panel
+   --  has Go Ahead clear and asks the user to press
+   --  ATTN. As Go Ahead is not set, ATTN is the only
+   --  key the terminal will allow to be pressed.
+   --
+
+   procedure To_Physical (
+      V         : Test_Break_Handler;
+      Bytes_Out : in out Byte_Vectors.Vector);
+
+   procedure From_Physical (
+      V        : in out Test_Break_Handler;
+      Bytes_In : Byte_Vectors.Vector);
+
+   procedure Break (V : in out Test_Break_Handler);
+
+   procedure Initialize (V : in out Test_Break_Handler);
+
+   procedure Set_RX_TX (
+      V  : in out Test_Break_Handler;
+      RX : access Buffer_Queues.Queue;
+      TX : access Buffer_Queues.Queue);
+
+end Test_Break_Handlers;
