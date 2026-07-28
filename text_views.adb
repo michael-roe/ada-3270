c399157a68d0bd96b2f6af3ed51b14f02dc49841
Michael Roe <michael.roe@github.com>
2026-07-28 15:19:28 +0100

    Text_Views: Use Panel_Elements.Text_Line

--- text_views.adb
+++ text_views.adb
@@ -27,11 +27,7 @@ package body Text_Views is
 
       Panel_Elements.Box_Top (0, P'Access, Bytes_Out);
 
-      Code_Page_310.Append (Bytes_Out, Box_Drawing.Vertical);
-      P.Append (Bytes_Out, " ");
-      P.Append (Bytes_Out, Lines.To_Wide_String (V.Title));
-      IBM_3270_Orders.Set_Buffer_Address (Bytes_Out, 79, 1);
-      Code_Page_310.Append (Bytes_Out, Box_Drawing.Vertical);
+      Panel_Elements.Text_Line (1, P'Access, V.Title, Bytes_Out);
 
       Panel_Elements.Horizontal_Rule (2, P'Access, Bytes_Out);
 
