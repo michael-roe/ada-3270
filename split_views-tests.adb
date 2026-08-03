1f362c99352cd71ac6c3971ec5d039378b08a5a1
Michael Roe <michael.roe@github.com>
2026-08-03 02:39:18 +0100

    Split_Views.Tests: Added Test_Enter

--- split_views-tests.adb
+++ split_views-tests.adb
@@ -1,12 +1,45 @@
 with AUnit.Assertions; use AUnit.Assertions;
 with Ada.Containers; use type Ada.Containers.Count_Type;
+with Ada.Wide_Text_IO;
 with Lines;
 with Byte_Vectors;
 with Line_Vectors;
 with IBM_3270;
+with IBM_3270_Orders;
 with Buffer; use type Buffer.Byte;
+with output_stream;
+with Code_Page_500;
 
 package body Split_Views.Tests is
+
+   P : aliased Code_Page_500.Page_500;
+
+   Field_Count : Natural;
+
+   Cursor_X : Natural;
+
+   Cursor_Y : Natural;
+
+   Cursor_X_2 : Natural;
+
+   Cursor_Y_2 : Natural;
+
+   procedure Update_Field (X : Natural; Y : Natural) is
+   begin
+
+      if Field_Count = 0 then
+         Cursor_X := X;
+         Cursor_Y := Y;
+      elsif Field_Count = 1 then
+         Cursor_X_2 := X;
+         Cursor_Y_2 := Y;
+      end if;
+
+      Field_Count := Field_Count + 1;
+
+   end Update_Field;
+
+   procedure Parse is new Output_Stream.Parse (Update_Field => Update_Field);
 
    procedure Test_Short_Read (T : in out Test_Cases.Test_Case'Class) is
       V : Split_View;
@@ -24,12 +57,47 @@ package body Split_Views.Tests is
 
    end Test_Short_Read;
 
+   procedure Test_Enter (T : in out Test_Cases.Test_Case'Class) is
+      V : Split_View;
+      Bytes_Out : Byte_Vectors.Vector;
+      Bytes_In : Byte_Vectors.Vector;
+      L : Lines.Bounded_Wide_String;
+   begin
+
+      V.To_Physical (Bytes_Out);
+
+      Field_Count := 0;
+      Parse (Bytes_Out);
+
+      Bytes_In.Append (IBM_3270.AID_Enter);
+      IBM_3270_Orders.Append_Buffer_Address (Bytes_In, 0, 0);
+      IBM_3270_Orders.Set_Buffer_Address (Bytes_In,
+         Cursor_X + 1,
+         Cursor_Y);
+      P.Append (Bytes_In, "H");
+      IBM_3270_Orders.Set_Buffer_Address (Bytes_In,
+         Cursor_X_2 + 1,
+         Cursor_Y_2);
+      P.Append (Bytes_In, "Hello World!");
+
+      V.From_Physical (Bytes_In);
+
+      Assert (V.Get_Option = 3, "Get_Option should return 3");
+      Lines.Set_Bounded_Wide_String (L, "Hello World!");
+      Assert (Lines.To_Wide_String (V.Edit (0)) = "Hello World!",
+         "Edit window should be Hello World!");
+
+   end Test_Enter;
+
    procedure Register_Tests (T : in out Split_View_Test) is
       use AUnit.Test_Cases.Registration;
    begin
 
       Register_Routine (T, Test_Short_Read'Access,
          "Test_Short_Read");
+
+      Register_Routine (T, Test_Enter'Access,
+         "Test_Enter");
 
    end Register_Tests;
 
--- split_views-tests.ads
+++ split_views-tests.ads
@@ -7,6 +7,8 @@ package Split_Views.Tests is
 
    procedure Test_Short_Read (T : in out Test_Cases.Test_Case'Class);
 
+   procedure Test_Enter (T : in out Test_Cases.Test_Case'Class);
+
    procedure Register_Tests (T : in out Split_View_Test);
 
    function Name (T : Split_View_Test) return Message_String;
