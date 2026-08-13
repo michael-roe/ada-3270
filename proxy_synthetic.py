88eec0323ba2b2de2649719bf0b83790dede1d9f
Michael Roe <michael.roe@github.com>
2026-08-13 21:49:40 +0100

    proxy_synthetic.py: Check for error from server

--- proxy_synthetic.py
+++ proxy_synthetic.py
@@ -68,43 +68,53 @@ while True:
 
   response = requests.post(url, headers=headers, json=payload, stream=True)
 
-  done = False
-  role = ""
-  content = ""
-  reasoning_content = ""
+  if response.status_code != requests.codes.ok:
+    error_msg = response.json()
+    print(error_msg["error"])
+    f.write(json.dumps(error_msg["error"]))
+    f.write("\n")
+    f.flush()
 
-  for line in response.iter_lines():
-    print("#", end="")
-    sys.stdout.flush()
-    line = line.decode("utf-8")
-    if line.startswith("data: "):
-      data = line[6:]
-      if data == "[DONE]":
-        break
-      chunk = json.loads(data)
-      choices = chunk.get("choices",[]);
-      if choices:
-        delta = choices[0].get("delta",{});
-        if "role" in delta:
-          role = delta["role"]
-        if "reasoning_content" in delta:
-          reasoning_delta = delta["reasoning_content"]
-          if not reasoning_delta is None:
-            reasoning_content = reasoning_content + reasoning_delta
-        if "content" in delta:
-          content_delta = delta["content"]
-          if not content_delta is None:
-            content = content + content_delta
+  else:
 
-  print("Role:", role)
-  print("Received:", content)
-
-  history.append({"role": role, "content": content})
-
-  log_file.write(json.dumps({"role": role, "content": content, "reasoning_content": reasoning_content}))
-  log_file.write("\n")
-  log_file.flush()
-
-  f.write(json.dumps(content))
-  f.write("\n")
-  f.flush()
+    done = False
+    role = ""
+    content = ""
+    reasoning_content = ""
+  
+    for line in response.iter_lines():
+      print("#", end="")
+      sys.stdout.flush()
+      line = line.decode("utf-8")
+      if line.startswith("data: "):
+        data = line[6:]
+        if data == "[DONE]":
+          break
+        chunk = json.loads(data)
+        choices = chunk.get("choices",[]);
+        if choices:
+          delta = choices[0].get("delta",{});
+          if "role" in delta:
+            role = delta["role"]
+          if "reasoning_content" in delta:
+            reasoning_delta = delta["reasoning_content"]
+            if not reasoning_delta is None:
+              reasoning_content = reasoning_content + reasoning_delta
+          if "content" in delta:
+            content_delta = delta["content"]
+            if not content_delta is None:
+              content = content + content_delta
+  
+    print("Role:", role)
+    print("Received:", content)
+  
+    history.append({"role": role, "content": content})
+  
+    log_file.write(json.dumps({"role": role, "content": content,
+      "reasoning_content": reasoning_content}))
+    log_file.write("\n")
+    log_file.flush()
+  
+    f.write(json.dumps(content))
+    f.write("\n")
+    f.flush()
